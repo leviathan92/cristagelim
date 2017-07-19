1446f993a25b60daf087f7452d4e3e257b59894e
leviathan92 <hollywood-1992@hotmail.com>
2017-07-18 19:57:07 -0500

    mejoras del modulo de creacion de usuarios: 1) ahora permita crear un usuario con un area. 2) correccion package areas

--- src/java/org/modulos/usuarios/AreasControlador.java
+++ src/java/org/modulos/usuarios/AreasControlador.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.controladores.admin.usuarios;
+package org.modulos.usuarios;
 
 import java.util.List;
 import javax.annotation.PostConstruct;
